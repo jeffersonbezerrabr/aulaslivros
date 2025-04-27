89c440bcba391c5143beeff9569860b9fce961ce
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-04-26 21:22:11 -0300

    Exercício 3.8

---
+++ exe3.8.py
@@ -0,0 +1,7 @@
+#Exercicio da página 113
+
+print("Conversor de metros para milímetros")
+
+metros = int(input("Digite o valor em metros: "))
+milímetros = metros * 1000
+print(f"{milímetros} Milímetros")
