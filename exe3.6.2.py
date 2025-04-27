c3d3cbbe327d656d79bf1a2dbafcf5e5bd5a361a
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-04-26 21:19:53 -0300

    Exercicios 3.6.2 e 3.7

---
+++ exe3.6.2.py
@@ -0,0 +1,7 @@
+nome = "João"
+idade = 22
+grana = 51.34
+
+print(f"{nome} tem {idade} anos e R${grana} no bolso.")
+print(f"{nome:12} tem {idade:03} anos e R${grana:5.2f} no bolso.")
+print(f"{nome:<12s} tem {idade:<3} anos e R${grana:5.2f} no bolso.")
---
+++ exe3.7.py
@@ -0,0 +1,11 @@
+#Exercicio da página 113
+
+'''anos = int(input("Anos de serviço: "))
+valor_por_ano = float(input("Valor por ano: "))
+bônus = anos * valor_por_ano
+print(f"Bônus de R$ {bônus:5.2f}")'''
+
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite mais um número: "))
+soma = numero1 + numero2
+print(f"A soma de {numero1} + {numero2} é: {soma}")
