79cee3130822d90966cd91dbb5c75b64e6e189f6
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-05-03 21:24:35 -0300

    Programas 4.1 e 4.2

---
+++ programa4.2.py
@@ -0,0 +1,15 @@
+#Programa 4.2: Carro novo ou velho, dependendo da idade - Página 120
+
+#Recebe a idade do carro do usuário
+idade = int(input("Digite a idade do seu carro: "))
+
+#Primeira comparação: Compara se a idade do carro é menor ou igual a 3
+if idade <= 3:
+    #Mostra o resultado
+    print("Seu carro é novo")
+
+#Segunda comparação: Compara se a idade do carro é maior que 3
+if idade > 3:
+    #Mostra o resultado
+    print("Seu carro é velho")
+    
