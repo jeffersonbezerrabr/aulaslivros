ebd8cb930c6330e4d75952fc89f31a2c85cc96a5
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-05-15 17:23:11 -0300

    Exercício 4.15

---
+++ exe4.15.py
@@ -0,0 +1,15 @@
+#Exercício 4.15 - Página 132
+
+#Reescreva o programa a seguir com if-elif-else.
+
+hora = int(input("Digite a hora atual: "))
+
+if hora < 12:
+    print("Bom dia!")
+    
+elif hora >= 12 and hora <= 18:
+    print("Boa tarde!")
+    
+else:
+    print("Boa noite!")
+    
