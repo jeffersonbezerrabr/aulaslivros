a07e5d3e80fcd4a83a5ed358cd6d1a17e56567ac
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-05-14 21:47:09 -0300

    Exercício 4.13

---
+++ exe4.13.py
@@ -0,0 +1,12 @@
+#Exercício 4.13 - Página 131
+
+#No programa a seguir, inverta as linhas do if e else, negando a condição.
+#Adicione as linhas necessárias para faze-lo funcionar em Pyhton.
+
+a = 1
+b = 2
+
+if not a > b:
+    print("b é maior que a")
+else:
+    print("a é maior que b")
