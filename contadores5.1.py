e7190886c66d8cd1a1aedb35ca11bd2b831469db
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-05-16 21:35:36 -0300

    Contadores

---
+++ contadores5.1.py
@@ -0,0 +1,21 @@
+#Contadores 5.1 - Página 139
+
+#fim = int(input("Digite o ultimo número a imprimir: "))
+#x = 1
+#while x <= fim:
+#    print(x)
+#    x = x + 1
+ 
+
+#fim = int(input("Digite o ultimo número a imprimir: "))
+#x = 0
+#while x <= fim:
+#    if x % 2 == 0:
+#        print(x)
+#    x = x + 1
+
+fim = int(input("Digite o ultimo número a imprimir: "))
+x = 0
+while x <= fim:
+    print(x)
+    x = x + 2
