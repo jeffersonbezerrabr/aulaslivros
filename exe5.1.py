ff73fbe33950b575389b83049da6b32dffcef2c5
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-05-16 21:34:27 -0300

    Exercício 5.1

---
+++ exe5.1.py
@@ -0,0 +1,14 @@
+#Exercício 5.1 - Página 138
+
+#Modifique o programapara exibir os números de 1 a 100
+
+#x = 1
+
+#while x <= 3:
+#   print(x)
+#    x = x + 1
+
+x = 1
+while x <= 100:
+    print(x)
+    x = x + 1
