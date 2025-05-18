bc3187c7e7f35b7d9d7baa89759385a949891953
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-05-18 20:55:21 -0300

    Exercício 5.2

---
+++ exe5.2.py
@@ -0,0 +1,15 @@
+#Exercício 5.2 - Página 138
+
+#Modifique o programapara exibir os números de 50 a 100
+
+#x = 1
+
+#while x <= 3:
+#   print(x)
+#    x = x + 1
+
+x = 50
+
+while x <=100:
+    print(x)
+    x = x + 1
