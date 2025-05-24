8ef0b0de781d271b2fd3818d92c329ed0f547c36
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-05-24 16:37:16 -0300

    Exercício 5.4

---
+++ exe5.4.py
@@ -0,0 +1,17 @@
+#Exercício 5.4 - Página 140
+
+#Modifique o exercício anterior para imprimir de 1 até o número
+#digitado pelo usuário, mas, dessa vez, apenas os números ímpares
+
+'''fim = int(input("Digite o ultimo número a imprimir: "))
+x = 0
+while x <= fim:
+    print(x)
+    x = x + 2'''
+
+fim = int(input("Digite o ultimo número a imprimir: "))
+x = 1
+while x <= fim:
+    print(x)
+    x = x + 2
+    
