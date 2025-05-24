de6c3f31b321ce17fbbdea9bc69f1d133243391e
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-05-24 16:37:53 -0300

    Programa 5.3

---
+++ programa5.3.py
@@ -0,0 +1,11 @@
+#Programa 5.3: Tabuada sem repetições aninhadas - Página 147
+
+tabuada = 1
+numero = 1
+
+while tabuada <=10:
+    print(f"{tabuada} x {numero} = {tabuada * numero}")
+    numero += 1
+    if numero == 11:
+        numero = 1
+        tabuada += 1
