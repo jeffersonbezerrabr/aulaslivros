c384247ecbac533a65436809c3ffd3d6486ce8b0
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-05-26 20:45:54 -0300

    Exercício 5.5

---
+++ exe5.5.py
@@ -0,0 +1,16 @@
+#Exercício 5.5 - Página 140
+
+#Reescreva o programa anterior para escrever os 10 primeiros
+#multiplos de 3.
+
+'''fim = int(input("Digite o ultimo número a imprimir: "))
+x = 0
+while x <= fim:
+    print(x)
+    x = x + 2'''
+    
+fim = int(input("Digite o ultimo número a imprimir: "))
+x = 3
+while x <= fim:
+    print(x)
+    x = x + 3
