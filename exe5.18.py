41d9b6f50ee2d7fb16921e38f80d5bf200ac504e
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-06-21 19:01:50 -0300

    Exercício 5.18

---
+++ exe5.18.py
@@ -0,0 +1,32 @@
+#Exercício 5.18 - Página 146
+
+#Modifique o programa 5.1 para trabalhar com cédulas de R$ 100.
+
+valor = int(input("Digite o valor a pagar: "))
+
+cedulas = 0
+atual = 100
+apagar = valor
+
+while True:
+    if atual <= apagar:
+        apagar = apagar - atual
+        cedulas = cedulas + 1
+    else:
+        print(f"{cedulas} cédula(s) de R${atual}")
+        if apagar == 0:
+            break
+        
+        if atual == 100:
+            atual = 50
+        elif atual == 50:
+            atual = 20
+        elif atual == 20:
+            atual = 10
+        elif atual == 10:
+            atual = 5
+        elif atual == 5:
+            atual = 1
+            
+        cedulas = 0
+        
