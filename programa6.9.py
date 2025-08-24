0ae583088088d76083150ec38dd7e317d38787e7
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-08-23 22:35:00 -0300

    Programa 6.9

---
+++ programa6.9.py
@@ -0,0 +1,16 @@
+#Programa 6.9 - Pesquisa Sequencial - Página 166
+
+L = [15, 7, 27, 39]
+p = int(input("Digite o valor a procurar: "))
+achou = False
+x = 0
+
+while x < len(L):
+    if L[x] == p:
+        achou = True
+        break
+    x += 1
+if achou:
+    print(f"{p} achado na posição {x + 1}")
+else:
+    print(f"{p} não encontrado")
