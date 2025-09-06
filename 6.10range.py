bb5dc684e73c6d7bd94b67463002c4f5633e8fca
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-09-05 22:22:54 -0300

    Range.py

---
+++ 6.10range.py
@@ -0,0 +1,26 @@
+# 6.10 Range - Página 169
+
+for v in range(10):
+    print(v)
+    
+print()
+
+for v in range(20):
+    print(v)
+    
+print()
+
+#for v in range(20000):
+#    print(v)
+
+for v in range(5, 8):
+    print(v)
+print()
+
+for v in range(0,10,2):
+    print(v)
+print()
+
+for t in range(3,33,3):
+    print(t, end=" ")
+    
