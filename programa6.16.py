8ac914233a4c60017b093f47edb7001347e692f7
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-10-05 22:55:58 -0300

    Programa 6.16

---
+++ programa6.16.py
@@ -0,0 +1,7 @@
+#Programa 6.16 - Listas com elememtos de tipos diferentes - Página 175
+
+produto1 = ["maçã", 10, 0.30]
+produto2 = ["pera", 5, 0.75]
+produto3 = ["kiwi", 4, 0.98]
+
+print(produto1, produto2, produto3)
