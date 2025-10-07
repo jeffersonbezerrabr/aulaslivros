5262b86b90e4d849c4eaaa4ad534fc7fad2e401e
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-10-06 22:21:21 -0300

    Dicionarios

---
+++ 6.17dicionarios.py
@@ -0,0 +1,35 @@
+#Dicionarios - Página 181
+
+'''tabela = {"Alface": 0.45, 
+          "Batata": 1.20,
+          "Tomate": 2.30,
+          "Feijão": 1.50}
+
+print(tabela["Alface"])
+print(tabela["Tomate"])
+print()
+print(tabela)
+print()
+tabela["Tomate"] = 2.50
+print()
+print(tabela["Tomate"])
+print()
+tabela["Cebola"] = 1.20
+print(tabela)'''
+
+tabela = {"Alface": 0.45,
+        "Batata": 1.20,
+        "Tomate": 2.30,
+        "Feijão": 1.50}
+print("Manga" in tabela)
+False
+print("Batata" in tabela)
+True
+print("=-=" * 25)
+
+tabela = {"Alface": 0.45,
+        "Batata": 1.20,
+        "Tomate": 2.30,
+        "Feijão": 1.50}
+print(tabela.keys())
+print(tabela.values())
