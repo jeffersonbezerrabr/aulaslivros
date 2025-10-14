7e48913e4eff1e358280e84885a00af28c06852f
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-10-13 22:24:35 -0300

    Programa 6.20

---
+++ programa6.20.py
@@ -0,0 +1,20 @@
+#Programa 6.20 - Ordenação pelo metodo de bolhas - Página 176
+
+L = [7, 4, 3, 12, 8]
+fim = 5 # Usamos a variável fim para marcar a qntd de elementos da lista.
+while fim > 1:
+    trocou = False
+    x = 0
+    while x < (fim - 1):
+        if L[x] > L[x + 1]:
+            trocou = True
+            temp = L[x]
+            L[x] = L[x + 1]
+            L[x + 1] = temp
+        x += 1
+    if not trocou:
+        break
+    fim -= 1
+for e in L:
+    print(e)
+    
