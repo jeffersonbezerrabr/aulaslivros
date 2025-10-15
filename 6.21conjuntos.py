8c9aaff187a27daac5a73d8a9108c134c3391c07
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-10-14 23:01:29 -0300

    Conjuntos

---
+++ 6.21conjuntos.py
@@ -0,0 +1,21 @@
+#6.21 - Conjuntos(set) - Página 193
+
+a = set()
+a.add(1)
+a.add(2)
+a.add(3)
+
+print(a)
+
+a.add(0)
+a.add(-1)
+print(a)
+
+print(1 in a)
+print(-2 in a)
+
+b = set([2, 3])
+print(b)
+
+print(len(a))
+print(len(b))
