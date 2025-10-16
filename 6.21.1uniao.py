e6897588a4bfffcec8bd681d9a658675ce1bcaab
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-10-15 22:34:36 -0300

    União

---
+++ 6.21.1uniao.py
@@ -0,0 +1,20 @@
+#6.21.1 - União - Página 194
+
+#A união dos conjuntos é realizada pelo operador | e resulta
+#em um novo conjunto com todos os elementos
+
+a = {0, 1, 2, 3, -1}
+b = {2, 3}
+c = set([4, 5, 6])
+
+print(a | b)
+
+print (a | c)
+
+print("-_*" * 25)
+
+A = {1, 2, 3, 6}
+B = {2, 4, 5, 6}
+
+print(A | B)
+print(A.union(B))
