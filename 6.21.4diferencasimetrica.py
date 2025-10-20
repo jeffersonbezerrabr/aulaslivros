974766560f3df61364045b1b53e5285cc1e90f53
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-10-19 22:45:38 -0300

    Diferença Assimetrica

---
+++ 6.21.4diferencasimetrica.py
@@ -0,0 +1,11 @@
+#6.21.4 - Diferença simetrica - Página 196
+
+#A diferença simetrica é inversa da diferença, ou seja.
+#resulta nos elementos que não são comuns aos dois conjuntos.
+
+A = {1, 2, 3, 6}
+B = {2, 4, 5, 6}
+
+print(A ^ B)
+
+#Elementos unicos de cada conunto
