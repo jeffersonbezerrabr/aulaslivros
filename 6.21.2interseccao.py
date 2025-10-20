ccd4353665b2a94a16984291fe56a43a344157a0
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-10-19 22:44:46 -0300

    Interseção

---
+++ 6.21.2interseccao.py
@@ -0,0 +1,11 @@
+#6.21.2 - Interseccao - Página 195
+
+#A intersecção é a operação que retorna apenas os elementos
+# em comum entre os dois conjuntos.
+
+
+A = {1, 2, 3, 6}
+B = {2, 4, 5, 6}
+
+print(A & B)
+print(A.intersection(B))
