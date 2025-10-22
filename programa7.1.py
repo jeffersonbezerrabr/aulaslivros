b8f1de388b34e8357081d6f9f6f76514b1e33579
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-10-21 22:00:30 -0300

    Programa 7.1

---
+++ programa7.1.py
@@ -0,0 +1,9 @@
+# Programa 7.1 - Pesquisa de todas as ocorrências - Página 205
+
+s = "um tigre, dois tigres, três tigres"
+p = 0
+while (p > -1):
+    p = s.find("tigre", p)
+    if p >= 0:
+        print(f"Posição: {p}")
+        p += 1
