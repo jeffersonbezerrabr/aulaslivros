17a8e9be2e7e634ed2767dd527ae4771df6a97a0
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-10-28 23:13:44 -0300

    Exercício 7.2 r2

---
+++ exe7.2.r.2.py
@@ -0,0 +1,19 @@
+# Exercício 7.2 - Página 206
+
+# Escreva um programa que leia duas strings e gere uma tereira
+# com os caracteres comuns às duas strings lidas.
+
+primeira = input("Digite a primeira string: ")
+segunda = input("Digite a segunda string: ")
+
+terceira = ''
+
+for letra in primeira:
+    if letra in segunda and letra not in terceira:
+        terceira += letra
+        
+if terceira == '':
+    print("Não há elementos em comum")
+
+else:
+    print(f"Elementos em comum: {terceira}")
