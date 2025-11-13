71a3893b0095dcdd1e11c460da6b919632ae08a8
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-11-12 22:25:05 -0300

    Exercício 7.7 r

---
+++ exe7.7.r.py
@@ -0,0 +1,16 @@
+# Exercício 7.7 - Página 207
+
+# Escreva um programa que peça ao usuário que digite uma frase
+# e imprima quantas vogais ela contém. 
+# Não considere maiúsculas e minúsculas como diferentes. 
+# 
+# Exemplo: uma frase como “A casa” deve imprimir três “as”.
+
+vogais = "aeiou"
+
+frase = input("Digite uma frase: ").lower().strip()
+
+for vogal in vogais:
+    contador_vogais = frase.count(vogal)
+    if contador_vogais > 0:
+        print(f"{vogal} aparece {contador_vogais} vezes")
