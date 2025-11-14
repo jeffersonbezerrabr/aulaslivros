8f6d4a9106b2290569ed7abb24331df01d041942
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-11-13 22:14:54 -0300

    Exercício 7.8

---
+++ exe7.8.py
@@ -0,0 +1,14 @@
+# Exercício 7.8 - Página 209
+
+# Escreva um programa para exibir todas as palavras de uma frase. 
+# Considere que uma palavra termina com um espaço em branco 
+# ou quando a string terminar. 
+# Exemplo: “O rato roeu a roupa” deve imprimir 5.
+
+frase = input("Digite uma frase: ")
+
+palavras = frase.split()
+
+for palavra in palavras:
+    print(palavra)
+print("Número de palavras:", len(palavras))
