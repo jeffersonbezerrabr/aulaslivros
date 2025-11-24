dc8af60118f37e35eb73b474f9449b3580a6bd16
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2025-11-23 21:46:12 -0300

    Validação por tipo de conteudo

---
+++ 7.8_Validacao_por_tipo_de_conteudo.py
@@ -0,0 +1,23 @@
+s = "125"
+p = "Alô Mundo"
+j = "alomundo"
+
+print(f"{s} é isalnum? {s.isalnum()}")
+print(f"{p} é isalnum? {p.isalnum()}")
+print(f"{j} é isalnum? {j.isalnum()}")
+
+print()
+
+print(f"{s} é isalpha? {s.isalpha()}")
+print(f"{p} é isalpha? {p.isalpha()}")
+print(f"{j} é isalpha? {j.isalpha()}")
+
+print()
+
+n = "771"
+m = "10.4"
+b = "-10"
+
+print(f"{n} é isdigit? {n.isdigit()}")
+print(f"{m} é isdigit? {m.isdigit()}")
+print(f"{b} é isdigit? {b.isdigit()}")
