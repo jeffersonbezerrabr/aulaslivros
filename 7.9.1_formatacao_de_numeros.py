faa669541f09ba39ef449253dfb3278c39a4a734
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2025-12-01 22:50:25 -0300

    Formatação de números

---
+++ 7.9.1_formatacao_de_numeros.py
@@ -0,0 +1,21 @@
+print("{0:05}".format(5))
+
+print("{0:*=7}".format(32))
+
+print("{0:*^11}".format("123"))
+
+print("{0:*<11}".format("123"))
+
+print("{0:*>11}".format("123"))
+
+print("{0:10,}".format(7532))
+
+print("{0:10.5f}".format(1500.31))
+
+print("{0:10,.5f}".format(1500.31))
+
+print("{0:+10} {1:-10}".format(5, -6))
+
+print("{0:-10} {1: 10}".format(5, -6))
+
+print("{0: 10} {1:+10}".format(5, -6))
