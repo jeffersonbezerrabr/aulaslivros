95f8271bb792c50e1aadaa185ca3f3547ce865f2
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-02-22 17:53:51 -0300

    Programa 8.3

---
+++ programa8.3.py
@@ -0,0 +1,11 @@
+# Programa 8.3: Cálculo do fatorial - Página 228
+
+def fatorial(n):
+    fat = 1
+    while n > 1:
+        fat *= n
+        n -= 1
+    return fat
+
+print(fatorial(3))
+print(fatorial(4))
