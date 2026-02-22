3c073dc251e5cd0fdf2eb4f68574d34f95d0eb39
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-02-22 17:54:14 -0300

    Exercicio 8.3r

---
+++ exe8.3.r.py
@@ -0,0 +1,14 @@
+# Exercício 8.3 - Página 225
+
+#Escreva uma função que receba o lado de um quadrado e retorne sua área (A = lado²).
+
+# Valores esperados:
+
+# area_quadrado(4) == 16
+# area_quadrado(9) == 81
+
+def area_quadrado(x):
+    return x ** 2
+
+print(area_quadrado(4))
+print(area_quadrado(9))
