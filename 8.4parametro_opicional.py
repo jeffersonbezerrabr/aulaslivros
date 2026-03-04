dd761c6a6d9c86b02b0690abc535934be1fc3fc5
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-03-03 21:37:21 -0300

    Exercício 8.4 e Parametro opcional

---
+++ 8.4parametro_opicional.py
@@ -0,0 +1,13 @@
+# # def barra():
+# #     print("*"*40)
+    
+# barra()
+
+def barra(n=40, caractere="*"):
+    print(caractere * n)
+    
+barra()
+
+barra(10)
+
+barra(10, "-")
---
+++ exe8.4.py
@@ -0,0 +1,14 @@
+# Exercício 8.4 - Página 225
+
+#Escreva uma função que receba a base e a altura de um triângulo e retorne sua área (A = (base x altura) / 2).
+
+# Valores esperados:
+
+# area_triangulo(6, 9) == 27
+# area_triangulo(5, 8) == 20
+
+def area_triangulo(b, a):
+    return b * a / 2
+
+print(area_triangulo(6, 9))
+print(area_triangulo(5, 8))
