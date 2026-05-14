e15c89a0bf4bc19f9f4320f4ff4739b23774fadb
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-05-13 23:14:47 -0300

    Exercicio 8.4.r2

---
+++ exe8.4.r2.py
@@ -0,0 +1,12 @@
+# Escreva uma função que receba a base e a altura de um triangulo
+# e retorne sua área (A = (base x altura) / 2).
+
+# Valores esperados:
+    # area_triangulo(6, 9) == 27
+    # area_triangulo(5, 8) == 20
+    
+def area_triangulo(b, a):
+    return (b * a) / 2
+
+print(area_triangulo(6, 9))
+print(area_triangulo(5, 8))
