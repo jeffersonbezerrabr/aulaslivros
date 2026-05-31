71e8f9d55869b47989b8bf1873f7243273040ff7
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-05-30 22:46:37 -0300

    Exercicio_2.3

---
+++ exercicio_2.3.py
@@ -0,0 +1,10 @@
+# Exercício 2.3 - Página 75
+# Faça um programa que exiba seu nome na tela
+
+nome = "Jefferson"
+idade = 38
+profissao_atual = "Suporte"
+profissao_futura = "Desenvolvedor"
+
+print("""Meu Nome é {}, tenho {} anos. Atualmente trabalho como {}, mas estou estudando para ser {}"""
+.format(nome, idade, profissao_atual, profissao_futura))
