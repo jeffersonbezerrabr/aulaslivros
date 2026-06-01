491fb7141a300b6deb290f621a7bf4ba452a1535
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-05-31 22:22:53 -0300

    Exercício 2.5

---
+++ exercicio_2.5.py
@@ -0,0 +1,11 @@
+# Exercício 2.5 - Página 75
+
+# Escreva um programa que calcule a soma de três variáveis e imprima o resultado na tela.
+
+a = 1
+b = 3
+c = 11
+
+soma = (a + b + c)
+
+print(soma)
