7d1a0e2dab11989f47ce1b028487cccfe886b27f
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-05-31 22:23:05 -0300

    Exercício 2.6

---
+++ exercicio_2.6.py
@@ -0,0 +1,12 @@
+# Exercício 2.6 - Página 75
+# Modifique o exercício 2.2 de forma que ele calcule um aumento de 15% para um salário de R$ 750.
+
+salario = 750
+aumento = 15 / 100
+
+print(salario + (salario * aumento))
+
+c = 5
+f = 5.0
+
+print(c == f)
