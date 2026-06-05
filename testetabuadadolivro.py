c9e11bf525f1f19065b2662becf1ba4e16672243
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-06-04 22:44:13 -0300

    Teste tabuada do livro

--- testetabuadadolivro.py
+++ testetabuadadolivro.py
@@ -10,21 +10,25 @@ Menu
 5 - Sair
 
 """)
-    opção = int(input("Escolha uma opção:"))
-    if opção == 5:
-        break
-    elif opção >= 1 and opção < 5:
-        n = int(input("Tabuada de:"))
-        x = 1
-        while x <= 10:
-            if opção == 1:
-                print(f"{n} + {x} = {n + x}")
-            elif opção == 2:
-                print(f"{n} - {x} = {n - x}")
-            elif opção == 3:
-                print(f"{n} / {x} = {n / x:5.4f}")
-            elif opção == 4:
-                print(f"{n} x {x} = {n * x}")
-            x = x + 1
-    else:
+    try:
+        opção = int(input("Escolha uma opção: "))
+        if opção == 5:
+            break
+        elif opção < 1:
+            print("Precisa digitar de 1 a 5")
+            continue
+        elif opção >= 1 and opção < 5:
+            n = int(input("Tabuada de:"))
+            x = 1
+            while x <= 10:
+                if opção == 1:
+                    print(f"{n} + {x} = {n + x}")
+                elif opção == 2:
+                    print(f"{n} - {x} = {n - x}")
+                elif opção == 3:
+                    print(f"{n} / {x} = {n / x:5.4f}")
+                elif opção == 4:
+                    print(f"{n} x {x} = {n * x}")
+                x = x + 1
+    except ValueError:
         print("Opção inválida!")
