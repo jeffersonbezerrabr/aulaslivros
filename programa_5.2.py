12082a10985c6916c548bcbc9576a5fb5634213b
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-06-10 22:37:42 -0300

    Programa_5.2

---
+++ programa_5.2.py
@@ -0,0 +1,11 @@
+# Programa 5.2: Tabuada com repetições aninhadas - Página 147
+
+tabuada = 1
+
+while tabuada <= 10:
+    numero = 1
+    
+    while numero <= 10:
+        print(f"{tabuada} x {numero} = {tabuada * numero}")
+        numero += 1
+    tabuada += 1
