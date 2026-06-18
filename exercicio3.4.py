fadf30349d5736409af1dd847fc3b0aa1c72b50b
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-06-17 22:57:59 -0300

    Exercicio3.4.r

---
+++ exercicio3.4.py
@@ -0,0 +1,8 @@
+# Exercício 3.4 - Página 100
+
+# Escreva um expressão para determinar se uma pessoa deve ou não pagar imposto.
+# Considee que pagam imposto pessoas cujo salário é maior que R$ 1200
+
+salario = 5000
+
+print(salario > 1200)
