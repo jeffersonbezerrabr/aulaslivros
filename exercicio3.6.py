5a89bd11f974f69710164d154344c9145dc6e908
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-06-17 22:58:18 -0300

    Exercicio3.6.r

---
+++ exercicio3.6.py
@@ -0,0 +1,15 @@
+# Exercício 3.6 - Página 100
+
+# Escreva uma expressão que será utilizada para decidir se um aluno foi aprovado ou não.
+# Para ser aprovado, todas as médias do aluno devem ser maiores ou iguais a 7.
+# Considere que o aluno cursa apenas três materias e que as notas estão armazenadas
+# nas seguintes variáveis:
+
+# materia1, materia2, materia3
+
+materia1 = 7
+materia2 = 8
+materia3 = 6
+
+print(materia1 >= 7 and materia2 >= 7 and materia3 >= 7)
+
