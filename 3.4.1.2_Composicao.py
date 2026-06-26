bfb467d35888fb3d139ffe9065a793005a3bfdd8
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-06-25 22:46:27 -0300

    3.4.1.2 - Composição

---
+++ 3.4.1.2_Composicao.py
@@ -0,0 +1,25 @@
+x = 2
+
+frase = "Heitor tem %d anos" %x
+
+print(frase)
+
+# %d = Int
+# %s = Strings
+# %f = Float
+
+idade = 22
+
+print("[%d]" % idade)
+
+print("[%03d]" % idade)
+
+print("[%09d]" % idade)
+
+print("[%3d]" % idade)
+
+print("[%-3d]" % idade)
+
+print("[%-5d]" % idade)
+
+
