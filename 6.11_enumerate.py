5b4508c70d5e367fb243f2834ded6aafe681cc3b
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-06-29 22:28:13 -0300

    6.11 Enumerate

---
+++ 6.11_enumerate.py
@@ -0,0 +1,17 @@
+# L = [5, 9, 13]
+# x = 0
+# for e in L:
+#     print(f"[{x}] {e}")
+#     x += 1
+    
+# L = [5, 9, 13]
+
+# for x, e in enumerate(L):
+#     print(f"[{x}] {e}")
+
+L = [5, 9, 13]
+
+for z in enumerate(L):
+    x, e = z
+    print(f"[{x}] {e}")
+    print(z)
