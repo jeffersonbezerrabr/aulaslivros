23a1e72623bbda4508e7889787009ce75d12064d
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-06-29 22:27:41 -0300

    5.1 Contadores

---
+++ 5.1_contadores.py
@@ -0,0 +1,21 @@
+# fim = int(input("Digite o ultimo número a imprimir: "))
+
+# x = 0
+
+# while x <= fim:
+#     if x% 2 == 0:
+#         print(x)
+#     x += 1
+
+# x = 0
+
+# while x <= fim:
+#     print(x)
+#     x += 2
+
+n = int(input("Tabuada de: "))
+x = 1
+
+while x <= 10:
+    print(n + x)
+    x += 1
