456fd3eddb297b8b609a1f950146b223db18a345
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-06-30 23:17:02 -0300

    Exercício 4.13.r

---
+++ exercicio4.13.py
@@ -0,0 +1,11 @@
+# No programa a seguir, inverta as linhas do if e else, negando a condição.
+# Adicione as linhas necessárias para faze-lo funcionar em Python.
+
+a = 5
+b = 0
+
+if a <= b:
+    print("b é maior que a")
+
+else:
+    print("a é maior que b")
