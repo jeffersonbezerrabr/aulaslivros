ba9da2c4109946e0b437419d94b278023ad37819
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-07-19 22:39:12 -0300

    Programa_6.10

---
+++ programa_6.10.py
@@ -0,0 +1,13 @@
+# Programa 6.10: Transformação de range em uma lista
+
+L = list(range(100, 1100, 50))
+
+print(L)
+
+print(type(L))
+print()
+
+for c in range(100, 1100, 50):
+    print(c,end=" ")
+print()  
+print(type(c))
