bfc0860d826915b336962dd4bd009403cc9ab0d9
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-07-26 23:41:01 -0300

    Programa_6.15

---
+++ programa_6.15.py
@@ -0,0 +1,9 @@
+# Programa 6.15: Impressão de uma lista de strings, letra a letra - Página 174
+
+L = ["maçãs", "peras", "kiwis"]
+
+for s in L:
+    for letra in s:
+        print(letra)
+    print()
+        
