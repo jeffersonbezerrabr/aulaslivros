87e8d48fd2fe80578f6126ac5ed720eda809e94a
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-07-27 22:53:03 -0300

    Programa_6.17

---
+++ programa_6.17.py
@@ -0,0 +1,9 @@
+# Programa 6.17: Listas de listas - Página 175
+
+produto1 = ["Maçã", 10, 0.30]
+produto2 = ["Pêra", 5, 0.75]
+produto3 = ["Kiwi", 4, 0.98]
+
+compras = [produto1, produto2, produto3]
+
+print(compras)
