1e7b6416a1148071eb3a1a5ab3f4807d2640d7de
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-08-03 22:49:01 -0300

    Exercício_7.7

---
+++ exercicio_7.7.py
@@ -0,0 +1,20 @@
+"""Exercício 07-07:
+
+Escreva um programa que peça ao usuário que digite uma frase e imprima 
+quantas vogais ela contém. Não considere maiúsculas e minúsculas como diferentes. 
+Exemplo: uma frase como “A casa” deve imprimir três “as”."""
+
+vogais = "aeiou"
+
+contador = {}
+
+frase = input("Digite uma frase: ").lower()
+
+for letra in frase:
+    if letra in vogais:
+        if letra in contador:
+            contador[letra] += 1
+        else:
+            contador[letra] = 1
+        
+print(contador)
