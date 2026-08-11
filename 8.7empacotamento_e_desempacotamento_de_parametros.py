4180415d60ad0504a042c2419eeae5a3915d30c9
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-08-11 18:42:31 -0300

    8.7 Empacotamento e desempacotamento de parametros

---
+++ 8.7empacotamento_e_desempacotamento_de_parametros.py
@@ -0,0 +1,15 @@
+
+# def soma(a, b):
+#     print(a + b)
+    
+# L = [2, 3]
+
+# soma(*L)
+
+def barra(n=10, c="*"):
+    print(c * n)
+    
+L = [[5, "-"], [10, "*"], [5], [6, "."]]
+
+for e in L:
+    barra(*e)
