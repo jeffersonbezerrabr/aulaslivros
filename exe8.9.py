ba89cc78423ead55eec21ae79bdf632fef916c53
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-08-18 22:34:48 -0300

    Exercício 8.9

---
+++ exe8.9.py
@@ -0,0 +1,12 @@
+# Rastreie o Programa 8.6 e compare o seu resultado com o apresentado - Página 234
+
+# O programa calcula o fatorial de 4
+# Pelas mensagens de saída impressas e pelo rastreamento do programa,
+# podemos concluir que o fatorial de 4 é calculado com chamadas recursivas
+# na linha: fat = n * fatorial(n-1)
+#
+# Como a chamada do fatorial precede a impressão da linha Fatorial de,
+# podemos visualizar a sequencia em forma de pilha, onde o cálculo é feito de fora
+# para dentro: Cálculo do fatorial de 4, 3 , 2 e 1
+# para então prosseguir na linha seguinte, que faz a impressão dos resultados:
+# fatorial de 1,2,3,4
